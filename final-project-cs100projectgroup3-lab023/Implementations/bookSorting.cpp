642b5565e8aff296ead33e064523243dc6a11a1b
sshen070 <165928605+sshen070@users.noreply.github.com>
2025-03-26 17:12:26 -0700

    Add files via upload

---
+++ final-project-cs100projectgroup3-lab023/Implementations/bookSorting.cpp
@@ -0,0 +1,24 @@
+#include <vector>
+#include <Library.h>
+
+using namespace std;
+
+
+void Library::sortByAlphabet(const vector<Book> bookCollection&) {
+	sort(bookCollection.begin(), bookCollection.end();
+
+}
+
+void Library::sortByRating(const vector<Book> bookCollection&){
+	sort(bookCollection.averageRating, bookCollection->ReviewList->averageRating.end());
+}
+
+void Library::sortByPopularity(const vector<Book> bookCollection&) {
+	sort(bookCollection.popularity.begin(), bookCollection.popularity.begin());
+}
+
+void Library::sortByAuthor(const vector<Book> bookCollection&) {
+	sort(bookCollection.author.begin(), bookCollection.author.begin());
+}
+
+
